70930a00ec5011651875e1464d701a4c42609c0a
SaiSandeep23 <125615373+SaiSandeep23@users.noreply.github.com>
2023-03-17 21:34:12 -0500

    Add files via upload
    assignment 4

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const reserveTable = ()=>{
+    $('#reserveTableModal').modal('show');
+}
+
+const closeReserveModal = ()=>{
+    $('#reserveTableModal').modal('hide');
+}
+
+const openLoginModal=()=>{
+    $('#LoginModal').modal('show');
+}
+
+const closeLoginModal=()=>{
+    $('#LoginModal').modal('hide');
+}
